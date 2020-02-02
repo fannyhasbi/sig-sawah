c1e369b9c0981502839593492bae3ce86565c401
fannyhasbi <fnyhsbi@gmail.com>
2020-02-02 21:36:37 +0700

    move into admin page

--- application/controllers/SawahGIS.php
+++ application/controllers/SawahGIS.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SawahGisAdmin extends CI_Controller {
+class SawahGIS extends CI_Controller {
   public function index(){
-    $this->load->view('sawah_gis/sawah_index');
+    
   }
 
 }
